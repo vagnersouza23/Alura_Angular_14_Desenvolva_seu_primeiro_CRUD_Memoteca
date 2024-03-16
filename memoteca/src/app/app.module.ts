b291e1d85d072d29fdb63ba6069bd391fb557e07
Vagner Souza <66963813+vagnersouza23@users.noreply.github.com>
2024-03-16 20:12:03 -0300

    Aula02-finalizada

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -16,7 +17,8 @@ import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensam
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  criarPensamento(){
+    alert('Novo pensamento criado!')
+  }
+
+  cancelar(){
+    alert('Novo pensamento cancelado!')
+  }
+
+
+
+
+
 
 }
